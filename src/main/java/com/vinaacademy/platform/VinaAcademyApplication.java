3e35e6d4536e82cfcd7f0f54a4dbf9c10c0b991f
Nguyen Huu Loc <locn562836@gmail.com>
2025-02-11 10:09:30 +0700

    spring initializr

---
+++ src/main/java/com/vinaacademy/platform/VinaAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.vinaacademy.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinaAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinaAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinaacademy/platform/VinaAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinaacademy.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinaAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
