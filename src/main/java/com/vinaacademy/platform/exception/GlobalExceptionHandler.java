15d474e50195b11a635f366a81c68046554546da
Nguyen Huu Loc <locn562836@gmail.com>
2025-02-19 17:01:09 +0700

    feat: add authentication and CORS configuration, implement custom error responses

---
+++ src/main/java/com/vinaacademy/platform/exception/GlobalExceptionHandler.java
@@ -0,0 +1,83 @@
+package com.vinaacademy.platform.exception;
+
+import com.vinaacademy.platform.feature.common.response.ApiResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@ControllerAdvice
+@ResponseBody
+@Order(HIGHEST_PRECEDENCE)
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({BadRequestException.class})
+    public ResponseEntity<Object> badRequest(BadRequestException e) {
+        logger.error(e.getMessage(), e.getCause());
+        return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        logger.error(e.getMessage(), e.getCause());
+
+        List<String> errors = e.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        return ResponseEntity.badRequest()
+                .body(ApiResponse.error(String.join(", ", errors)));
+    }
+
+    @ExceptionHandler({
+            UnauthorizedException.class,
+            AuthenticationException.class,
+            InternalAuthenticationServiceException.class,
+            BadCredentialsException.class
+    })
+    public ResponseEntity<Object> unauthorizedRequest(Exception e) {
+        logger.error(e.getMessage(), e.getCause());
+        String message = e.getCause() instanceof UsernameNotFoundException ?
+                e.getCause().getMessage() : e.getMessage();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.error(message));
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<Object> accessDenied(AccessDeniedException e) {
+        logger.error(e.getMessage(), e.getCause());
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(ApiResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> ex(Exception e) {
+        logger.error(e.getMessage(), e.getCause());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error(e.getMessage()));
+    }
+}
