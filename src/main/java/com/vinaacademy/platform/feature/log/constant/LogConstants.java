bd30a5251dd33db24cc2e9f4d7c1fbd021f73ddc
Nguyen Huu Loc <locn562836@gmail.com>
2025-02-21 18:24:06 +0700

    feat: implement logging feature with LogRepository, LogService, and LogController

---
+++ src/main/java/com/vinaacademy/platform/feature/log/constant/LogConstants.java
@@ -0,0 +1,23 @@
+package com.vinaacademy.platform.feature.log.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class LogConstants {
+    public static final String CATEGORY_KEY = "category";
+    public static final String PRODUCT_KEY = "product";
+    public static final String ORDER_KEY = "order";
+    public static final String LOGIN_KEY = "login";
+    public static final String USER_KEY = "user";
+    public static final String ROLE_KEY = "role";
+
+
+    public static final String ADD_ACTION = "add";
+    public static final String UPDATE_ACTION = "update";
+    public static final String DELETE_ACTION = "delete";
+    public static final String DELETE_ALL_ACTION = "delete_all";
+    public static final String LOGIN_ACTION = "login";
+    public static final String ASSIGN_PASSWORD_ACTION = "assign_password";
+    public static final String CHANGE_PASSWORD_ACTION = "change_password";
+
+}
---
+++ src/main/java/com/vinaacademy/platform/feature/log/dto/LogDto.java
@@ -0,0 +1,24 @@
+package com.vinaacademy.platform.feature.log.dto;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.vinaacademy.platform.feature.common.dto.BaseDto;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LogDto extends BaseDto {
+    private Long id;
+    private String name;
+    private String event;
+    private String description;
+    private String username;
+    private String oldData;
+    private String newData;
+}
