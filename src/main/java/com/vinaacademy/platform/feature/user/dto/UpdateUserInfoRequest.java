655a171c0b9032516e726343bc8764d56e837b17
trihung987 <51873366+trihung987@users.noreply.github.com>
2025-04-26 02:07:02 +0700

    minor2

--- src/main/java/com/vinaacademy/platform/feature/user/dto/UpdateUserInfoRequest.java
+++ src/main/java/com/vinaacademy/platform/feature/user/dto/UpdateUserInfoRequest.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserInfoRequest {
-    @Size(min = 2, max = 100, message = "Họ tên phải có độ dài từ 2 đến 100 ký tự")
+    @Size(min = 2, max = 50, message = "Họ tên phải có độ dài từ 2 đến 50 ký tự")
     private String fullName;
     
     @Size(max = 1500, message = "Mô tả không được vượt quá 1500 ký tự")
