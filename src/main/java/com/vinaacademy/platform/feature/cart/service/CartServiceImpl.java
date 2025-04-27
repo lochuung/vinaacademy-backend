648f061e932b537553e2fa0f245ffd1f3177c0dc
Nguyen Huu Loc <locn562836@gmail.com>
2025-04-27 20:50:15 +0700

    fix(cart): handle null cart case in getCart method

--- src/main/java/com/vinaacademy/platform/feature/cart/service/CartServiceImpl.java
+++ src/main/java/com/vinaacademy/platform/feature/cart/service/CartServiceImpl.java
@@ -17,59 +17,66 @@ import java.util.UUID;
 
 @Service
 public class CartServiceImpl implements CartService {
-	@Autowired
-	private CartRepository cartRepository;
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private CartMapper cartMapper;
-	@Autowired
-	private CouponRepository couponRepository;
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CartMapper cartMapper;
+    @Autowired
+    private CouponRepository couponRepository;
 
-	@Override
-	public CartDto getCart(UUID userId) {
-		Cart cart = cartRepository.findByUserId(userId)
-				.orElseThrow(() -> BadRequestException.message("Không tìm thấy ID của người này"));
-		CartDto cartDto = cartMapper.toDTO(cart);
-		return cartDto;
-	}
+    @Override
+    public CartDto getCart(UUID userId) {
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElse(null);
+        CartDto cartDto = cartMapper.toDTO(cart);
 
-	@Override
-	public CartDto createCart(CartRequest request) {
-		if (cartRepository.existsByUserId(request.getUser_id()))
-			throw BadRequestException.message("Cart của ID người dùng này đã tồn tại");
+        if (cart == null) {
+            cartDto = createCart(CartRequest.builder()
+                    .user_id(userId)
+                    .coupon_id(null)
+                    .build());
+        }
+        return cartDto;
+    }
 
-		User userp = userRepository.findById(request.getUser_id())
-				.orElseThrow(() -> BadRequestException.message("Không tìm thấy ID người dùng này"));
-		Cart cart = Cart.builder().user(userp).coupon(null).build();
-		cartRepository.save(cart);
-		CartDto cartDto = cartMapper.toDTO(cart);
-		return cartDto;
-	}
+    @Override
+    public CartDto createCart(CartRequest request) {
+        if (cartRepository.existsByUserId(request.getUser_id()))
+            throw BadRequestException.message("Cart của ID người dùng này đã tồn tại");
 
-	@Override
-	public CartDto updateCart(CartRequest request) {
-		Cart cart = cartRepository.findByUserId(request.getUser_id())
-				.orElseThrow(() -> BadRequestException.message("Không tìm thấy Cart của ID người dùng này"));
-		User userp = userRepository.findById(request.getUser_id())
-				.orElseThrow(() -> BadRequestException.message("Không tìm thấy ID người dùng này"));
-		Coupon coupon = null;
-		if (request.getCoupon_id() != null) {
-			coupon = couponRepository.findById(request.getCoupon_id())
-					.orElseThrow(() -> BadRequestException.message("Không tìm thấy coupon"));
-		}
-		cart.setCoupon(coupon);
-		cart.setUser(userp);
-		cartRepository.save(cart);
-		CartDto cartDto = cartMapper.toDTO(cart);
-		return cartDto;
-	}
+        User userp = userRepository.findById(request.getUser_id())
+                .orElseThrow(() -> BadRequestException.message("Không tìm thấy ID người dùng này"));
+        Cart cart = Cart.builder().user(userp).coupon(null).build();
+        cartRepository.save(cart);
+        CartDto cartDto = cartMapper.toDTO(cart);
+        return cartDto;
+    }
 
-	@Override
-	public void deleteCart(CartRequest request) {
-		Cart cart = cartRepository.findByUserId(request.getUser_id())
-				.orElseThrow(() -> BadRequestException.message("Không tìm thấy Cart của ID người dùng này"));
-		cartRepository.delete(cart);
-	}
+    @Override
+    public CartDto updateCart(CartRequest request) {
+        Cart cart = cartRepository.findByUserId(request.getUser_id())
+                .orElseThrow(() -> BadRequestException.message("Không tìm thấy Cart của ID người dùng này"));
+        User userp = userRepository.findById(request.getUser_id())
+                .orElseThrow(() -> BadRequestException.message("Không tìm thấy ID người dùng này"));
+        Coupon coupon = null;
+        if (request.getCoupon_id() != null) {
+            coupon = couponRepository.findById(request.getCoupon_id())
+                    .orElseThrow(() -> BadRequestException.message("Không tìm thấy coupon"));
+        }
+        cart.setCoupon(coupon);
+        cart.setUser(userp);
+        cartRepository.save(cart);
+        CartDto cartDto = cartMapper.toDTO(cart);
+        return cartDto;
+    }
+
+    @Override
+    public void deleteCart(CartRequest request) {
+        Cart cart = cartRepository.findByUserId(request.getUser_id())
+                .orElseThrow(() -> BadRequestException.message("Không tìm thấy Cart của ID người dùng này"));
+        cartRepository.delete(cart);
+    }
 
 }
