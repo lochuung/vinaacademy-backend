36326e7de03c089b619f436c0d98070ea453068d
Nguyen Huu Loc <locn562836@gmail.com>
2025-05-04 11:08:06 +0700

    feat(course): implement slug generation with retry logic and add SlugGeneratorHelper

--- src/main/java/com/vinaacademy/platform/configuration/AppConfig.java
+++ src/main/java/com/vinaacademy/platform/configuration/AppConfig.java
@@ -3,8 +3,10 @@ package com.vinaacademy.platform.configuration;
 import org.apache.tika.Tika;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
+@EnableRetry
 public class AppConfig {
     @Bean
     public Tika tika() {
